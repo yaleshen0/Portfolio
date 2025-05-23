8bafd3145a246af95f5141f9eded771d89a83a23
yaleshen0 <yaleshen0>
2025-05-22 21:08:02 -0400

    footer section

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -8,7 +8,6 @@ import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
-import Footer from "../components/footer/Footer";
 import Talks from "./talks/Talks";
 import Podcast from "./podcast/Podcast";
 import Education from "./education/Education";
@@ -64,7 +63,6 @@ const Main = () => {
             <Twitter />
             <Podcast />
             <Profile />
-            <Footer />
             <ScrollToTopButton />
           </>
         )}
